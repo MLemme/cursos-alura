2cfda39455b1e72f9ce54e16ada257048ee3ce43
Mauricio Lemme <mauricio.lemme@gmail.com>
2025-02-25 14:39:04 -0300

    CSS Flexbox e layouts responsivos Aula 1

--- curso-php/screen-match/index.php
+++ curso-php/screen-match/index.php
@@ -1,108 +1,23 @@
 <?php
-/* movidos para um arquivo só para funções
-function exibeMensagemLancamento(int $ano): void{
 
-    if ($ano > 2022) {
-        echo "Esse filme é um lançamento\n";
-    } elseif($ano > 2020 && $ano <= 2022) {
-        echo "Esse filme ainda é novo\n";
-    } else {
-        echo "Esse filme não é um lançamento\n";
-    }
-
-}
-
-function incluidoNoPlano(bool $planoPrime , int $anoLancamento): bool{
-    return $planoPrime || $anoLancamento < 2020;
-}
-*/
-#inclusão da nova classe
 require __DIR__ . "/src/Modelo/Filme.php";
-require __DIR__ . "/src/funcoes.php"; #dir traz o path absoluto
-
-#include é igual ao require no entanto não causa o erro fatal, não interrompe
+require __DIR__ . "/src/funcoes.php";
 
 echo "Bem-vindo(a) ao screen match!\n";
 
-$nomeFilme = "Top Gun - Maverick";
-
-$anoLancamento = 2022;
-
-$quantidadeDeNotas = $argc - 1;
-$notas = [];
-
-for ($contador = 1; $contador < $argc; $contador++) {
-    $notas[] = (float) $argv[$contador];
-}
-
-$notaFilme = array_sum($notas) / $quantidadeDeNotas;
-$planoPrime = true;
-
-$incluidoNoPlano = incluidoNoPlano($planoPrime,$anoLancamento);
-
-echo "Nome do filme: " . $nomeFilme . "\n";
-echo "Nota do filme: $notaFilme\n";
-echo "Ano de lançamento: $anoLancamento\n";
-
-exibeMensagemLancamento($anoLancamento);
-
-$genero = match ($nomeFilme) {
-    "Top Gun - Maverick" => "ação",
-    "Thor: Ragnarok" => "super-herói",
-    "Se beber não case" => "comédia",
-    default => "gênero desconhecido",
-};
-
-echo "O gênero do filme é: $genero\n";
-
-/*$filme = [
-    "nome" => "Thor: Ragnarok",
-    "ano" => 2021,
-    "nota" => 7.8,
-    "genero" => "super-herói",
-];*/
-
-$filme = criaFilme(
-    nome: "Thor: Ragnarok",
-    anoLancamento: 2021, 
-    nota: 7.8, 
-    genero: "super-herói"
-);
-#php permite nomear os parametros a partir das variaveis que foram criadas, impossibilitando assim falhas por parâmetros em ordem diferente da estrutura da função
-//Acesso quando retorno é um array associativo
-#echo $filme["ano"];
-//Acesso quando retorno é uma classe
-echo $filme->anoDeLancamento;
-
-//Tratamento de arquivos json
-#filmeComoStringJson = json_encode($filme);
-#função que encapsula todos os processos com arquivos para escrita, passando local e nome do arquivo e conteudo string
-#file_put_contents(__DIR__ . '\filme.json',$filmeComoStringJson);
-
+$filme = new Filme();
+$filme->defineAnoLancamento(2021);
 /*
-#demonstração da função sort que ordena arrays
-var_dump($notas);
-sort($notas);
-var_dump($notas);
-
-#busca pelo menor valor
-echo min($notas);
-#ou
-$menorNota = min($notas);
-var_dump($menorNota);
-
-#demonstração de manipulação de strings
-var_dump($filme['nome']);
-var_dump(strpos($filme['nome'],':')); #busca posição dentro da string
-$posicaoDoisPontos = strpos($filme['nome'],':');
-var_dump(substr($filme['nome'], 0, $posicaoDoisPontos)); #busca substring
+$filme->nome = 'Thor - Ragnarok';
+$filme->anoDeLancamento = 2021;
+$filme->genero = 'super-heroi';
 */
-/*
-echo json_encode($filme); #transforma uma variavel ou valor do php para o formato json
+$filme->avalia(10);
+$filme->avalia(6);
+$filme->avalia(7.8);
+$filme->avalia(8.2);
 
-var_dump(json_decode('{"nome":"Thor: Ragnarok","ano":2021,"nota":7.8,"genero":"super-her\u00f3i"}',true)); #esta função faz o caminho reverso da primiera e o segundo parametro garante que o retorno seja em formato de array associativo, ou dicionario de dados, a entrada é melhor com '' pois as "" do dado passam a não interfirir
-*/
+var_dump($filme);
 
-//comando para inicia um servidor local que vai sempre apontar para pasta public dentro do diretório do programa -> php -S localhost:8001 -t public
-
-//comando php -a - abre um terminal do php , terminal interativo
+echo $filme->media() . "\n";
+echo $filme->anoLancamento();
--- curso-php/screen-match/src/Modelo/Filme.php
+++ curso-php/screen-match/src/Modelo/Filme.php
@@ -1,8 +1,54 @@
 <?php
 
 class Filme {
-    public string $nome;
-    public int    $anoDeLancamento;
-    public float  $nota;
-    public string $genero;
+    private string $nome = 'Nome padrão';
+    private int    $anoDeLancamento = 2024;
+    private string  $genero = 'Genérico';
+    private float  $media; //$nota;
+    #quando public, se pode acessar a propriedade diretamente sem a necessidade de um método get
+    #torna a variavel interna a classe - modificador de acesso
+    private array $notas = []; //array criado vazio
+
+    public function avalia(float $nota): void
+    {
+        $this->notas[] = $nota;
+    }
+
+    public function media(): float {
+        $somaNotas = array_sum($this->notas);
+        $quantidadeNotas = count($this->notas);
+
+        return $somaNotas / $quantidadeNotas;
+    }
+    #método de acesso ou acessor - getter
+    public function anoLancamento(): int
+    {
+        return $this->anoLancamento;
+    }
+    #método definidor - setter
+    public function defineAnoLancamento(int $anoDeLancamento)
+    {
+        $this->anoLancamento = $anoDeLancamento;
+    }
+
+    public function nome(): int
+    {
+        return $this->nome;
+    }
+
+    public function defineNome(int $nome): void
+    {
+        $this->nome = $nome;
+    }
+
+    public function genero(): int
+    {
+        return $this->genero;
+    }
+
+    public function defineGenero(int $genero): void
+    {
+        $this->genero = $genero;
+    }
+
 }
